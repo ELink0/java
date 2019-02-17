c3a18228553eb079799d6ed99f3e74561580bb7e
Link0 <36975653+ELink0@users.noreply.github.com>
2019-02-17 17:28:53 -0300

    Alterado métodos
    Foi deixado comentado os métodos menos eficientes para identificação de nome (depende do caso). E foi adicionado textos para melhor interação com o usuário final.

--- NomeSobrenome.java
+++ NomeSobrenome.java
@@ -6,15 +6,15 @@ public class NomeSobrenome {
 	public static void main(String[] args) {
 		String nome = JOptionPane.showInputDialog("Digite seu nome");
 		Integer quantidadeCaracteresNome = nome.length();
-		System.out.println(quantidadeCaracteresNome);
+		System.out.println("Seu nome tem "+quantidadeCaracteresNome+" letras");
 		
 		String[] palavrasNome = nome.split(" ");
-		System.out.println(palavrasNome[0]);
-		for(int x=0;x<palavrasNome.length;x++) {
-			System.out.println(palavrasNome[x]);
-		}
-		for(String palavra:palavrasNome) {
-			System.out.println(palavra);
-		}
+		System.out.println("Seu primeiro nome é: "+palavrasNome[0]);
+//		for(int x=0;x<palavrasNome.length;x++) {
+//			System.out.println(palavrasNome[x]);
+//		}
+//		for(String palavra:palavrasNome) {
+//			System.out.println(palavra);
+//		}
 	}
 }

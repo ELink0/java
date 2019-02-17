20e7776b89452537ff4d466685d3d590de6426b5
Link Lulz <linkifpr@gmail.com>
2019-02-17 17:23:37 -0300

    Alterado o arquivo nomeSobrenome.java

---
+++ nomeSobrenome.java
@@ -0,0 +1,20 @@
+package controle;
+
+import javax.swing.JOptionPane;
+
+public class nomeSobrenome {
+	public static void main(String[] args) {
+		String nome = JOptionPane.showInputDialog("Digite seu nome");
+		Integer quantidadeCaracteresNome = nome.length();
+		System.out.println("Seu nome tem "+quantidadeCaracteresNome+" letras");
+		
+		String[] palavrasNome = nome.split(" ");
+		System.out.println("Seu primeiro nome é: "+palavrasNome[0]);
+//		for(int x=0;x<palavrasNome.length;x++) {
+//			System.out.println(palavrasNome[x]);
+//		}
+//		for(String palavra:palavrasNome) {
+//			System.out.println(palavra);
+//		}
+	}
+}

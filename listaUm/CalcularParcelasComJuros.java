05a9878b19c128e4556896503eae3f58dd715e43
Link0 <36975653+ELink0@users.noreply.github.com>
2019-02-19 20:09:15 -0300

    Código foi melhorada a identação

--- listaUm/CalcularParcelasComJuros.java
+++ listaUm/CalcularParcelasComJuros.java
@@ -3,16 +3,16 @@ package listaUm;
 import javax.swing.JOptionPane;
 
 public class CalcularParcelasComJuros {
-	public static void main(String[]args){
-		String valorString = JOptionPane.showInputDialog("Insira o valor do produto: ");
-		String mesesString = JOptionPane.showInputDialog("Insira a quantidade de meses: ");
-		
-		double valor = Double.parseDouble(valorString);
-		Integer meses = Integer.parseInt(mesesString);
-		
-		for(int x=0; x<meses; x++) {
-			valor = valor * (1.05);
-		}
-		System.out.println("O valor total é "+valor);
-	}
+ public static void main(String[] args) {
+  String valorString = JOptionPane.showInputDialog("Insira o valor do produto: ");
+  String mesesString = JOptionPane.showInputDialog("Insira a quantidade de meses: ");
+
+  double valor = Double.parseDouble(valorString);
+  Integer meses = Integer.parseInt(mesesString);
+
+  for (int x = 0; x < meses; x++) {
+   valor = valor * (1.05);
+  }
+  System.out.println("O valor total é " + valor);
+ }
 }

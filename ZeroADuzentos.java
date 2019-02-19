5d47718ef956e720c95b86679f43af5f164e0a2e
Link0 <36975653+ELink0@users.noreply.github.com>
2019-02-18 21:47:16 -0300

    Conta os números que terminam com 5 entre 200
    Conta os números que terminam com 5 entre 200 números

---
+++ ZeroADuzentos.java
@@ -0,0 +1,11 @@
+package listaUm;
+
+public class ZeroADuzentos {
+	public static void main(String args[]) {
+		String valor;
+		
+		for(int i=5; i < 200; i+=10) {
+				System.out.println(i);
+		}
+	}
+}

f18c58cb4fa5e3a9b8fe269edd1e64806f080cfb
Link0 <36975653+ELink0@users.noreply.github.com>
2019-03-18 22:29:34 -0300

    Update Conectar.java

--- conexaoBancoDeDados/Conectar.java
+++ conexaoBancoDeDados/Conectar.java
@@ -1,60 +1,83 @@
-package bancoDeDados;
-// Utilizado o repositório Maven do MySQL Connector ou Driver MySQL
-
+package testeBiblioteca;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.Statement;
-
 import javax.swing.JOptionPane;
 
-import bancoDeDados.Par;
+import controle.VerificarPar;
 
-public class Conectar {
+public class Projeto {
 	public static void main(String[] args) {
-		Par p = new Par();
 		
-		// url
 		String url = "jdbc:mysql://localhost:3306/banco?useTimezone=true&serverTimezone=UTC";
-		// usuário
+		
 		String user = "root";
-		// senha
 		
 		try {
-		Connection conn = DriverManager.getConnection(url, user, "");
+			
+		Connection conn = DriverManager.getConnection(url, user,"");
+		Statement st = conn.createStatement();
+		
 		
 		String resposta = "s";
 		while(resposta.equals("s")){
-			resposta = JOptionPane.showInputDialog("Deseja cadastrar mais dados? [s] ou [n]");
+			
 	
 			if (resposta.equals("n")){
 				break;
 			}
-			String nome = JOptionPane.showInputDialog("Insira seu nome");
-			String cpf = JOptionPane.showInputDialog("Insira seu cpf");
-			String nomeEstado = JOptionPane.showInputDialog("Insira o seu Estado");
-			String sigla = JOptionPane.showInputDialog("Insira a sigla do Estado");
+			String nome = JOptionPane.showInputDialog("Insira seu Nome");
+			String rg = JOptionPane.showInputDialog("Insira o seu rg");
+			String cpf = JOptionPane.showInputDialog("Insira o seu cpf");
+			String telefone = JOptionPane.showInputDialog("Insira seu telefone");
+			resposta = JOptionPane.showInputDialog("Insira sua resposta S ou N");
 			
 			
-			Statement st = conn.createStatement();
-			st.executeUpdate("INSERT INTO PESSOA(NOME,CPF) VALUES('"+nome+"','"+cpf+"')");
-			st.executeUpdate("INSERT INTO ESTADO(NOMEEST,SIGLA) VALUES('"+nomeEstado+"','"+sigla+"')");
 			
-			
-			ResultSet rs = st.executeQuery("SELECT * FROM PESSOA");
-			
-			String resultado = "";
-			
-			while(rs.next()) {
-				resultado = resultado + rs.getString("pedro")+"\n";
-			}
-			JOptionPane.showMessageDialog(null, resultado);
+			st.executeUpdate("INSERT INTO CLIENTE(NOME,RG,CPF,TELEFONE) VALUES('"+nome+"','"+rg+"','"+cpf+"','"+telefone+"')");
+	
+
 		}
 		
+		//-----------------------------------------------------------------------------
+//		ResultSet rs = st.executeQuery("SELECT * FROM CLIENTE");
+//		String resultado = "";
+//		while(rs.next()) {
+//			resultado += rs.getString("nome")+"\n";
+//		}
+//		JOptionPane.showMessageDialog(null, resultado);
+		//-------------------------EXCLUIR-----------------------------------------------
+		String nomeExcluir = JOptionPane.showInputDialog("Insira seu Nome para excluir: ");
+		st.executeUpdate("DELETE FROM CLIENTE WHERE NOME = '"+nomeExcluir+"'");
+		
+			
+		//---------------------------ALTERAR--------------------------------------------------
+		
+		String nomeAntigo = JOptionPane.showInputDialog("Insira o nome para ser alterado: ");
+		String nomeNovo = JOptionPane.showInputDialog("Insira o novo nome: ");
+		st.executeUpdate("UPDATE CLIENTE SET NOME = '"+nomeNovo+"' WHERE NOME = '"+nomeAntigo+"'");
+		
 
+//		
+		//-----------------------CONSULTAR------------------------------------------------------
+		ResultSet rs = st.executeQuery("SELECT NOME,RG,cpf,telefone FROM CLIENTE WHERE NOME = 'christian'");
+		String resultado = "";
+		while(rs.next()) {
+			resultado += rs.getString("nome")+" RG: ";
+			resultado += rs.getInt("rg")+" CPF: ";
+			resultado += rs.getInt("cpf")+" TELEFONE: ";
+			resultado += rs.getInt("telefone")+" \n";
+		}
+		JOptionPane.showMessageDialog(null, resultado);
+		
+		
+		
 		}catch(Exception e) {
 			e.printStackTrace();
+
 		}
 	}
 }
+//categoria, deseja inserir outro???, id nome descição

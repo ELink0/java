f5c8b82742b8085781ab196e775e3f960bd1c13f
Link0 <36975653+ELink0@users.noreply.github.com>
2019-03-26 22:35:40 -0300

    Add files via upload

---
+++ Spring/projetoaula/src/main/java/com/christian/projetoaula/controller/IndexController.java
@@ -0,0 +1,15 @@
+package com.christian.projetoaula.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+
+public class IndexController {
+	@RequestMapping("/calculadora")
+	public String index() {
+		return "calculadora";
+	}
+}

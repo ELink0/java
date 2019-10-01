90b7e4f58e11795d527e0b125141c2c016a03e1f
Link0 <36975653+ELink0@users.noreply.github.com>
2019-09-30 22:33:38 -0300

    Add files via upload

---
+++ src/principal/TelaAjuda.java
@@ -0,0 +1,12 @@
+package principal;
+
+import javax.swing.JInternalFrame;
+
+public class TelaAjuda extends JInternalFrame {
+	public TelaAjuda() {
+		super("Ajuda",true, true, true);
+		setLocation(700, 46);
+		setSize(550, 400);
+		setVisible(true);
+	}
+}
---
+++ src/principal/TelaJogoForca.java
@@ -0,0 +1,13 @@
+package principal;
+
+import javax.swing.JInternalFrame;
+
+public class TelaJogoForca extends JInternalFrame {
+	public TelaJogoForca() {
+		super("Jogo da Forca",true, true, true);
+		setLocation(500, 75);
+		setSize(550, 400);
+		setVisible(true);
+	}
+
+}

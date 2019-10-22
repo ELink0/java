ae205dc0d3e31521b438821dd66f939cff87c8bf
Link0 <36975653+ELink0@users.noreply.github.com>
2019-10-21 21:42:16 -0300

    v2

---
+++ pooJFrame/src/tela/tela/CalculadoraDeImc.java
@@ -0,0 +1,102 @@
+package tela;
+
+import java.awt.Color;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.text.DecimalFormat;
+ 
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JInternalFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JTextField;
+ 
+
+public class CalculadoraDeImc extends JInternalFrame{
+    private JTextField edtPeso;
+    private JTextField edtAltura;
+    private MeuCampo;
+    private JButton btnCalcula;
+    private JLabel lblPeso;
+    private JLabel lblAltura;
+    private JLabel lblResultado;
+ 
+    public CalculadoraDeImc(){
+    	super("Calculadora de IMC", true, true, true, true);
+        setConfig();
+    }
+ 
+
+    public void setConfig() {
+    	//JOptionPane.showMessageDialog(null,"teste");
+    	edtPeso = new JTextField();
+        edtAltura = new JTextField();
+        btnCalcula = new JButton("Calcular");
+        lblPeso = new JLabel("Peso:");
+        lblAltura = new JLabel("Altura:");
+        lblResultado = new JLabel("Resultado: ");
+         
+        btnCalcula.addActionListener(new EventoCalculaIMC());
+         
+        add(lblPeso);
+        add(lblAltura);
+        add(edtPeso);
+        add(edtAltura);
+        add(lblResultado);
+        add(btnCalcula);
+        
+        // Definições da janela
+        setSize(605, 150);
+        setLayout(new GridLayout(0, 2));
+        setVisible(true);
+    }
+ 
+    public class EventoCalculaIMC implements ActionListener{
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            try {
+ 
+                double peso = Double.parseDouble(edtPeso.getText());
+                double altura= Double.parseDouble(edtAltura.getText());
+                double imc = peso / Math.pow(altura, 2);
+ 
+                //objeto para formatar saida, com uma decimal
+                DecimalFormat df = new DecimalFormat("#0.0");
+ 
+                String resultado="Resultado: "+df.format(imc);
+                 
+                //abaixo comparo valor do imc e concatena a string resultado
+                if(imc < 18.5){
+                    resultado+=" Abaixo do peso";
+                }
+                else if(imc < 24.9){
+                    resultado+=" Peso ideal";
+                }
+                else if(imc < 29.9){
+                    resultado+=" Levemente acima do peso";
+                }
+                else if(imc < 34.9){
+                    resultado+=" Primeiro grau de obesidade";
+                }
+                else if(imc < 39.9){
+                    resultado+=" Segundo grau de obesidade";
+                }
+                else
+                    resultado+=" Obesidade mórbida";
+ 
+                lblResultado.setText(resultado);
+ 
+            }catch (ArithmeticException ar) {
+                JOptionPane.showMessageDialog(null, "Erro aritmético, causa: "+ar.getMessage());
+            } catch (Exception ex) {
+                JOptionPane.showMessageDialog(null, "Erro desconhecido, causa: "+ex.getMessage());
+            }
+        }
+    }
+ 
+    public static void main(String[] args) {
+        new CalculadoraDeImc();
+    }
+}
